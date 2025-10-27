58e351a1e81adf82372088f2ef2fb87677694c34
sixunderscore <152335583+sixunderscore@users.noreply.github.com>
2025-10-27 16:18:45 +0100

    fixed extended ascii set

--- src/main/java/net/sixunderscore/mocha2d/graphics/resources/text/TextData.java
+++ src/main/java/net/sixunderscore/mocha2d/graphics/resources/text/TextData.java
@@ -2,6 +2,6 @@ package net.sixunderscore.mocha2d.graphics.resources.text;
 
 public class TextData {
     public static final int FIRST_CHAR = 33;
-    public static final int LAST_CHAR = 256;
+    public static final int LAST_CHAR = 255;
     public static final int NUM_CHARS = LAST_CHAR - FIRST_CHAR + 1;
 }
