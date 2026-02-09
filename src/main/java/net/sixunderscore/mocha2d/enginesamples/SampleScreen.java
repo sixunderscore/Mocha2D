3bde7af54f0a5e173e4be0f07b9b9825ee7678fc
6___ <hommie.squid@gmail.com>
2026-02-10 00:13:49 +0100

    dam

--- src/main/java/net/sixunderscore/mocha2d/enginesamples/SampleScreen.java
+++ src/main/java/net/sixunderscore/mocha2d/enginesamples/SampleScreen.java
@@ -43,7 +43,7 @@ public class SampleScreen implements Screen {
             this.t += Window.getDeltaTime() * this.rotationScalar;
         }
 
-        batch.addSprite(this.logoTexture, Window.getWidth() / 2f - 150, Window.getHeight() / 2f - 150, 300, 300, MathUtils.lerp(0, MathUtils.TAU_f, this.t), Window.getWidth() / 2f, Window.getHeight() / 2f);
+        batch.addSprite(this.logoTexture, Window.getWidth() / 2f - 150, Window.getHeight() / 2f - 150, 300, 300, MathUtils.lerp(0, MathUtils.PI_TIMES_2, this.t), Window.getWidth() / 2f, Window.getHeight() / 2f);
         batch.addText(this.bitmapFont, "FPS: " + Window.getFpsCount(), 10, Window.getHeight() - 50, 1.1f);
     }
 }
